070136bb0325290142c4d2d6c81dbb256ed4a02b
Junrou Nishida <eulerdora.1810.mann@gmail.com>
2015-08-19 10:08:08 +0900

    [Add] Packaging task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
+'use strict';
+
+var _ = require('lodash');
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
+var packageJson = require('./package.json');
+var packager = require('electron-packager');
 
 gulp.task('html', function() {
    return gulp.src('src/**/*.html')
@@ -11,9 +16,11 @@ gulp.task('sass', function() {
       .pipe(gulp.dest('build/css'));
 });
 
-gulp.task('resources', function() {
-   return gulp.src('src/**/*.{png}')
-      .pipe(gulp.dest('build'));
+gulp.task('packageJson', function() {
+   var fs = require('fs');
+   var copied = _.cloneDeep(packageJson);
+   copied.main = 'main.js';
+   fs.writeFile('./build/package.json', JSON.stringify(copied));
 });
 
 gulp.task('scripts', function() {
@@ -39,11 +46,29 @@ gulp.task('watch', function() {
    gulp.watch('src/**/*.html', ['html']);
 });
 
-gulp.task('build', ['html', 'sass', 'scripts'], function() {
-
-});
+gulp.task('build', ['packageJson', 'html', 'sass', 'scripts']);
 
 function handleError(err) {
    console.log(err.toString());
    this.emit('end');
 }
+
+gulp.task('package', ['linux', 'win32', 'darwin'].map(function(platform) {
+   var taskName = 'package:' + platform;
+   console.log(platform);
+   gulp.task(taskName, ['build'], function(done) {
+      packager({
+         dir: './build',
+         name: 'pyon2-labeler',
+         arch: 'x64',
+         platform: platform,
+         out: './release/' + platform,
+         version: '0.30.1',
+         overwrite: true
+      }, function (err) {
+         done();
+      });
+   });
+   return taskName;
+}));
+
